b798ab3adc6d0bec9b1d8d93b073aaf3872a630c
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-09-18 14:49:57 -0300

    for reading the sompz file... not finished

--- sompz.py
+++ sompz.py
@@ -2,13 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 
-def open_sompz():
-    f = h5py.File('desy3_sompz_v050.h5')
-    df = f['catalog']['sompz']
+sompzcat = h5py.File('../cats/DESY3/desy3_sompz_v050.h5')['catalog']['sompz']
 
-    # sompz.keys() = ['pzdata','sheared_1m','sheared_1p','sheared_2m','sheared_2p']
-    ## pzdata.keys() = ['bin0','bin1','bin2','bin3','pz_c','pz_chat','z_high','z_low']
-    return df
 
-if __name__ = '__main__':
+
+
+
+if __name__ == '__main__':
     print('a')
