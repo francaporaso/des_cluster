48f6b3ba630eed1899efba2d0c4634185e84bbf4
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2025-10-04 20:48:44 -0300

    saving test

--- sompz.py
+++ sompz.py
@@ -1,26 +1,29 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import h5py
 from astropy.table import Table
+import time
 
-sompz = h5py.File('../cats/DESY3/desy3_sompz_v050.h5')
-sompz = sompz['catalog/sompz']
+def main():
+    sompz = h5py.File('../cats/DESY3/desy3_sompz_v050.h5')
+    sompz = sompz['catalog/sompz']
 
-metacal = Table.read('../cats/DESY3/desy3_unsheared.fits', memmap=True, format='fits')
-metacal = metacal[metacal['unsheared_flags']==0]
+    metacal = Table.read('../cats/DESY3/desy3_unsheared.fits', memmap=True, format='fits')
+    metacal = metacal[metacal['unsheared_flags']==0]
 
-flag_mask = np.isin(sompz['unsheared/coadd_object_id'], metacal['coadd_object_id'])
-bin0_mask = sompz['unsheared/bhat'][:] == 0
+    flag_mask = np.isin(sompz['unsheared/coadd_object_id'], metacal['coadd_object_id'])
+    bin0_mask = sompz['unsheared/bhat'][:] == 0
 
-cellid_bin0 = sompz['unsheared/cell_wide'][flag_mask&bin0_mask]
+    cellid_bin0 = sompz['unsheared/cell_wide'][flag_mask&bin0_mask]
 
-pz_bin0 = np.zeros(601)
-for i, cid in enumerate(cellid_bin0):
-    pz_bin0 += sompz['pzdata/pz_chat'][:][cid]
+    pz_bin0 = np.zeros(601)
+    for i, cid in enumerate(cellid_bin0):
+        pz_bin0 += sompz['pzdata/pz_chat'][:][cid]
 
-zrange = (sompz['pzdata/zlow'][:]+sompz['pzdata/zhigh'][:])*0.5
-plt.plot(zrange, pz_bin0)
-plt.savefig('bin0_test.png')
+    zrange = (sompz['pzdata/zlow'][:]+sompz['pzdata/zhigh'][:])*0.5
+
+    np.savetxt('pz_bin0.csv', pz_bin0)
 
 if __name__ == '__main__':
-    print('a')
+    tin = time.time()
+    main()
+    print(f'took {time.time()-tin} s')
