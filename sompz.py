ffe2a5ecf4d1dd330472eab4bbc9095c584c60c6
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-10-07 12:45:15 -0300

    testing sompz and metacal

--- sompz.py
+++ sompz.py
@@ -2,6 +2,7 @@ import numpy as np
 import h5py
 from astropy.table import Table
 import time
+import pandas as pd
 
 def main():
     sompz = h5py.File('../cats/DESY3/desy3_sompz_v050.h5')
@@ -9,19 +10,22 @@ def main():
 
     metacal = Table.read('../cats/DESY3/desy3_unsheared.fits', memmap=True, format='fits')
     metacal = metacal[metacal['unsheared_flags']==0]
+    j = np.sort(metacal['coadd_object_id'])
 
-    flag_mask = np.isin(sompz['unsheared/coadd_object_id'], metacal['coadd_object_id'])
-    bin0_mask = sompz['unsheared/bhat'][:] == 0
+    mask = np.isin(sompz['unsheared/coadd_object_id'], j)
+    df = pd.DataFrame({
+        'bhat':sompz['unsheared/bhat'][mask],
+        'cell_wide':sompz['unsheared/cell_wide'][mask],
+    })
+    chat_bin=[]
+    for i in range(4):
+        chat_bin.append(np.unique(df.query(f'bhat=={i}')['cell_wide'], return_counts=True))
 
-    cellid_bin0 = sompz['unsheared/cell_wide'][flag_mask&bin0_mask]
+    pchat_s = [chat_bin[i][1]/len(j) for i in range(4)]
+    pz_chat = [sompz['pzdata/pz_chat'][chat_bin[i][0]] for i in range(4)]
 
-    pz_bin0 = np.zeros(601)
-    for i, cid in enumerate(cellid_bin0):
-        pz_bin0 += sompz['pzdata/pz_chat'][:][cid]
-
-    zrange = (sompz['pzdata/zlow'][:]+sompz['pzdata/zhigh'][:])*0.5
-
-    np.savetxt('pz_bin0.csv', pz_bin0)
+    np.savetxt('test_mcal_sompz.dat', np.hstack(pchat_s, pz_chat))
+    
 
 if __name__ == '__main__':
     tin = time.time()
