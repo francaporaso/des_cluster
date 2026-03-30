fbdb39fd79047d9abf388a5d37abd155d9af3023
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-03-30 17:26:59 -0300

    first steps towards lensing

--- graw.py
+++ graw.py
@@ -1,40 +1,88 @@
 import numpy as np
 import h5py
 from astropy.cosmology import FlatLambdaCDM
-from astropy.constants import c, G, pc, M_sun
+#from astropy.constants import c, G, pc, M_sun
 from astropy.table import Table
 from scipy.integrate import simpson
 
-from funcs import sompzcat_load, metacal_load
+from funcs import eq2p2, get_masked_idx_fast
+from io import read_metacal, read_redmapper
+from nzsource import calculate_median, sigma_crit, lensing_efficiency, read_nzsource
 
 COSMO = FlatLambdaCDM(H0=100, Om0=0.3)
-SC_CTE = (c.value**2.0/(4.0*np.pi*G.value))*(pc.value/M_sun.value)*1.0e-6
 
-sompz = sompzcat_load()
-metacal = metacal_load()
+N = 10
+RIN, ROUT = 0.2, 15.0 #Mpc/h
+BINNING = 'log'
 
-pz_b = np.array(sompz['bin0'])
-z_s = np.arange(0.0,6.0,0.01)
+Source = read_metacal()
+Lenses = read_redmapper()
 
-def sigma_crit(z_l, z_s):
-    d_ls = COSMO.angular_diameter_distance_z1z2(z_l, z_s).value
-    d_l  = COSMO.angular_diameter_distance(z_l).value*pc.value*1.0e6
-    d_s  = COSMO.angular_diameter_distance(z_s).value
-    return np.where(d_ls>0, ((c.value**2.0)/(4.0*np.pi*G.value*d_l))*(d_s/d_ls)*(pc.value**2/M_sun.value), 0.0)
+if BINNING=='log':
+    binspace = np.geomspace
+elif BINNING=='lin':
+    binspace = np.linspace
+else:
+    raise ValueError('BINNING must be "log" or "lin".')
 
-def inverse_sigma_crit(z_l, z_s):
-    # eq 4 bocquet+2024
-    d_l  = COSMO.angular_diameter_distance(z_l).value
-    d_s  = COSMO.angular_diameter_distance(z_s).value
-    d_ls = COSMO.angular_diameter_distance_z1z2(z_l, z_s).value
-    return (1.0/SC_CTE)*(d_l/d_s)*np.max([0.0,d_ls])
+def mask_zbin():
+    pass
 
-def mean_sigma_crit_b(z_l, pz):
-    # eq 6 Bocquet+2024
-    'pz_b : ndarray = mean source redshift distribution as "function" of z_s'
-#    z_s = np.linspace(0.0,1.6,0.1) # source redshift range... CHANGE ACCORDINGLY
-    return simpson(pz*inverse_sigma_crit(z_l, z_s), x=z_s)
+def partial_profile(inp):
+
+    g_t_raw_num = np.zeros(N)
+    g_x_raw_num = np.zeros(N)
+    g_a_raw_den = np.zeros(N)
+    N_inbin = np.zeros(N)
+
+    ra0, dec0, z0, w_b0, w_b1, w_b2, w_b3 = inp
+
+    DEGxMPC = COSMO.arcsec_per_kpc_proper(z0).to('deg/Mpc').value
+    psi = DEGxMPC*ROUT
+
+    # get masked data
+    idx = get_masked_idx_fast(psi, ra0, dec0, z0)
+    catdata = Source[idx]
+
+    # calculate transformation to polar coords
+    rads, theta = eq2p2(
+        np.deg2rad(catdata['ra_gal']), np.deg2rad(catdata['dec_gal']),
+        np.deg2rad(ra0), np.deg2rad(dec0)
+    )
+
+    e1 = catdata['e_1']
+    e2 = -catdata['e_2']
+    R1 = catdata['r11']
+    R2 = catdata['r22']
+    #se usa el promedio entre ambos xq son muy similares ((R1-R2)/(0.5*(R1+R2)) < 0.1%)
+    R = 0.5*(R1+R2)
+
+    #get tangential ellipticities
+    cos2t = np.cos(2.0*theta)
+    sin2t = np.sin(2.0*theta)
+    et = -(e1*cos2t+e2*sin2t)
+    ex = (-e1*sin2t+e2*cos2t)
+
+    #get weights
+    w_s = catdata['unsheared_weights']
+    w_b = np.array([w_b0, w_b1, w_b2, w_b3])
+
+    #get redshift bins
+    zbin = mask_zbin()
+
+    ndots = binspace(RIN, ROUT, N+1)
+    dig = np.digitize((np.rad2deg(rads)/DEGxMPC), ndots)
+
+    for n_i in range(N):
+        m_i = dig == n_i+1
+        for b in range(4):  
+            g_t_raw_num[n_i] += w_b[b]*np.sum(et[m_i][zbin[b]]*w_s[m_i][zbin[b]])
+            g_x_raw_num[n_i] += w_b[b]*np.sum(ex[m_i][zbin[b]]*w_s[m_i][zbin[b]])
+            g_a_raw_den[n_i] += w_b[b]*np.sum(R[m_i][zbin[b]]*w_s[m_i][zbin[b]])
+            N_inbin[n_i] += np.count_nonzero(m_i)
+
+    return g_t_raw_num, g_x_raw_num, g_a_raw_den, N_inbin
+
 
 if __name__ == '__main__':
-    z_l=float(input())
-    print(mean_sigma_crit_b(z_l))
+    print('a')
