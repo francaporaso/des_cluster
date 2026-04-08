93466b02f33a09c2c3adc782100cdb1fed1bbb50
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-04-08 11:47:58 -0300

    debug plot

--- graw.py
+++ graw.py
@@ -128,13 +128,12 @@ def main():
 
     axes[0].scatter(r[g_t_raw > 0], g_t_raw[g_t_raw > 0], s=5, marker='o')
     axes[0].scatter(r[g_t_raw <= 0], np.abs(g_t_raw[g_t_raw <= 0]), s=5, marker='o', facecolor='none')
-
     axes[0].scatter(r[g_x_raw > 0], g_x_raw[g_x_raw > 0], s=5, marker='x', color='gray')
     axes[0].scatter(r[g_x_raw <= 0], np.abs(g_x_raw[g_x_raw <= 0]), s=5, marker='x', color='gray', facecolor='none')
+    axes[0].loglog()
 
     axes[1].scatter(r, N_inbin.sum(axis=0), c='green', s=5)
 
-    fig.loglog()
     fig.savefig('test_des.png')
 
 if __name__ == '__main__':
