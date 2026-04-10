7bef6814550110f922a677427765386f2048a1cb
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-04-10 16:45:15 -0300

    test: added dsigma calc, with an ansatz equation, derived from the voidlensing one and using neff from eq 9 of Grandis2024

--- graw.py
+++ graw.py
@@ -44,7 +44,10 @@ def get_masked_data(psi, ra0, dec0, z0, wb):
             wb[i] = 0.0
     return mask_sky, wb
 
-def partial_profile(inp):
+def partial_profile_gt_raw(inp):
+    '''
+    Profile of reduced shear g_t(r) as in eq. 6 of Grandis et al. (2024)
+    '''
 
     g_t_raw_num = np.zeros(NBINS)
     g_x_raw_num = np.zeros(NBINS)
@@ -99,8 +102,136 @@ def partial_profile(inp):
 
     return g_t_raw_num, g_x_raw_num, response_sum, n_eff_den, N_inbin
 
+def partial_profile_DeltaSigma(inp):
+    '''
+    Profile of proj. density contrast \Delta\Sigma(r).
+    No equation found in literature, intuitive derivation:
 
-def main():
+    $$
+        \Delta \Sigma = \frac{
+            \sum_{l=0}^{N_L} \sum_{b} \sum_{i \in b} w_{bl} w_{s,il} e_{t,il}/R_{il} }{
+            \sum_{l=0}^{N_L} N_{s,l}
+        }
+    $$
+
+    where
+    $$
+        N_{s,l} = \frac{
+            (\sum_{b} \sum_{i \in b} w_{bl} w_{s,il} R_{il} )^2 }{
+            \sum_{b} \sum_{i \in b} (w_{bl} w_{s,il} R_{il})^2
+        }
+    $$
+
+    and $w_{bl}$ is the lensing efficiency of bin $b$ over lens $l$, $w_{s,il}$ is the weight of source $i$ of lens $l$ and $R_{il}$ is the response of source $i$ of lens $l$.
+    '''
+
+    dsigma_t_sum = np.zeros(NBINS)
+    dsigma_x_sum = np.zeros(NBINS)
+    response_sum = np.zeros(NBINS)
+    n_bin = np.zeros(NBINS)
+    n_sl_sum = np.zeros(NBINS)
+
+    ra0, dec0, z0, *w_b = inp
+
+    DEGxMPC = COSMO.arcsec_per_kpc_proper(z0).to('deg/Mpc').value
+    psi = DEGxMPC*ROUT
+
+    # get masked data
+    mask, w_b = get_masked_data(psi, ra0, dec0, z0, w_b)
+    catdata = Source[mask]
+
+    # calculate transformation to polar coords
+    rads, theta = eq2p2(
+        np.deg2rad(catdata['ra_gal']), np.deg2rad(catdata['dec_gal']),
+        np.deg2rad(ra0), np.deg2rad(dec0)
+    )
+
+    #get weights
+    w_s = catdata['weight']
+
+    e1 = -catdata['e_1']
+    e2 = catdata['e_2']
+    R1 = catdata['r11']
+    R2 = catdata['r22']
+    #se usa el promedio entre ambos xq son muy similares
+    #((R1-R2)/(0.5*(R1+R2)) < 0.1%)
+    R = 0.5*(R1+R2)
+
+    #get weighted tangential ellipticities
+    cos2t = np.cos(2.0*theta)
+    sin2t = np.sin(2.0*theta)
+    et = (-e1*cos2t+e2*sin2t)*w_s
+    ex = (e1*sin2t+e2*cos2t)*w_s
+
+    ndots = binspace(RIN, ROUT, NBINS+1)
+    dig = np.digitize((np.rad2deg(rads)/DEGxMPC), ndots)
+
+    for n_i in range(NBINS):
+        m_i = dig == n_i+1
+        for b in range(4):
+            zbin = catdata['bhat'] == b
+            dsigma_t_sum[n_i] += w_b[b]*np.sum(et[m_i & zbin]/R[m_i & zbin])
+            dsigma_x_sum[n_i] += w_b[b]*np.sum(ex[m_i & zbin])/R[m_i & zbin])
+            response_sum[n_i] += w_b[b]*np.sum(R[m_i & zbin])
+            n_ls_sum[n_i] += w_b[b]**2 * np.sum(R[m_i & zbin]**2)
+            n_bin[n_i] += np.count_nonzero(m_i & zbin)
+
+    return dsigma_t_sum, dsigma_x_sum, response_sum, n_ls_sum, N_inbin
+
+def stack_dsigma():
+    savefile = 'test-des_dsigma'
+    l = Lenses[ (Lenses['lambda']>38.0) & (Lenses['lambda']<=55) & (Lenses['redshift']>0.19) & (Lenses['redshift']<=0.27) ]
+    print(f'nlenses = {len(l)}')
+
+    dsigma_t_sum = np.zeros((len(l), NBINS))
+    dsigma_x_sum = np.zeros((len(l), NBINS))
+    response_sum = np.zeros((len(l), NBINS))
+    n_ls_sum = np.zeros((len(l), NBINS))
+    N_inbin = np.zeros((len(l), NBINS))
+
+    for i, li in enumerate(l):
+        dsigma_t_sum[i,:], dsigma_x_sum[i,:], response_sum[i,:], n_ls_sum[i,:], N_inbin[i,:] = partial_profile(
+            [
+                li['ra_gal'],
+                li['dec_gal'],
+                li['redshift'],
+                li['wb_0'],
+                li['wb_1'],
+                li['wb_2'],
+                li['wb_3']
+            ]
+        )
+
+    response = np.sum(response_sum, axis=0)
+    n_eff = np.sum(response_sum**2/n_sl_sum, axis=0)
+
+    dsigma_t = np.sum(dsigma_t_sum, axis=0)/n_eff
+    dsigma_x = np.sum(dsigma_x_sum, axis=0)/n_eff
+    Nbin = np.sum(N_inbin, axis=0)
+
+    r = binspace(RIN, ROUT, NBINS)
+
+    np.savetxt(savefile+'.dat', np.vstack([r, dsigma_t, dsigma_x, n_eff]))
+
+    if PLOT:
+        fig, axes = plt.subplots(ncols=2, nrows=2, sharex=True, figsize=(5,6))
+
+        axes[0,0].scatter(r[dsigma_t > 0], dsigma_t[dsigma_t > 0], s=5, marker='o')
+        axes[0,0].scatter(r[dsigma_t <= 0], np.abs(dsigma_t[dsigma_t <= 0]), s=5, marker='o', edgecolor='b', facecolor='none')
+        axes[1,0].scatter(r[dsigma_x > 0], dsigma_x[dsigma_x > 0], s=5, marker='o', color='gray')
+        axes[1,0].scatter(r[dsigma_x <= 0], np.abs(dsigma_x[dsigma_x <= 0]), s=5, marker='o', edgecolor='gray', facecolor='none')
+        axes[0,0].loglog()
+        axes[1,0].loglog()
+
+        axes[0,1].scatter(r, Nbin, c='green', s=5)
+        axes[1,1].scatter(r, n_eff, c='green', s=5)
+        axes[0,1].loglog()
+        axes[1,1].loglog()
+
+        fig.savefig(savefile+'.png')
+
+
+def stack_gt_raw():
 
     l = Lenses[ (Lenses['lambda']>38.0) & (Lenses['lambda']<=55) & (Lenses['redshift']>0.19) & (Lenses['redshift']<=0.27) ]
     print(f'nlenses = {len(l)}')
@@ -132,7 +263,7 @@ def main():
 
     r = binspace(RIN, ROUT, NBINS)
 
-    np.savetxt('test_des.dat', np.vstack([r, g_t_raw, g_x_raw, n_eff, N_inbin]))
+    np.savetxt('test-des_gtraw.dat', np.vstack([r, g_t_raw, g_x_raw, n_eff, N_inbin]))
 
     if PLOT:
         fig, axes = plt.subplots(ncols=2, nrows=2, sharex=True, figsize=(5,6))
@@ -155,7 +286,7 @@ if __name__ == '__main__':
 
     print('Start'.center(15,'-'))
     t1 = time()
-    main()
+    stack_dsigma()
     print('End'.center(17,'-'))
     print(f'Took {time()-t1} s')
 
