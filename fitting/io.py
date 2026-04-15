b5f106b72163ce47edd47927c81dc3cde3d5d46a
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-04-15 17:02:13 -0300

    added dataclass for reading and storing the measured profile

---
+++ fitting/io.py
@@ -0,0 +1,69 @@
+import numpy as np
+from dataclasses import dataclass
+from astropy.io import fits
+from astropy.table import Table
+
+@dataclass
+class DataProfile:
+    redshift : np.float64
+    Njk : np.float64
+    R : np.ndarray
+    DSigma_t : np.ndarray
+    covDSt : np.ndarray
+    DSigma_x : np.ndarray
+    covDSx : np.ndarray
+    covS : np.ndarray
+    Sigma : np.ndarray
+
+    def plot_profile(self, observable='sigma', **kwargs):
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(1,1)
+        ax.set_xlabel('R')
+        if observable=='sigma':
+            ax.set_ylabel('$\\Sigma$')
+            ax.errorbar(self.R, self.Sigma, np.sqrt(np.diag(self.covS)),
+                         **kwargs)
+        elif observable=='delta_sigma':
+            ax.set_ylabel('$\\Delta\\Sigma$')
+            ax.errorbar(self.R, self.DSigma_t, np.sqrt(np.diag(self.covDSt)),
+                         **kwargs)
+            ax.errorbar(self.R, self.DSigma_x, np.sqrt(np.diag(self.covDSx)),
+                        fmt='x', **kwargs)
+        
+        fig.show()
+        return fig
+
+    def plot_cov(self, observable='sigma', **kwargs):
+        import matplotlib.pyplot as plt
+        if observable == 'sigma':
+            plt.imshow(self.covS)
+        elif observable == 'delta_sigma':
+            plt.imshow(self.covDSt)
+        else:
+            plt.imshow(self.covDSx)
+
+        plt.show()
+
+# the **kwargs requires giving the arg name when calling this function
+# ex: data = read_dataprofile_fits(name='myprofile.fits')
+# this is not going to work: data = read_dataprofile_fits('myprofile.fits').
+def read_dataprofile_fits(binning='lin', *args, **kwargs):
+    binspace = (np.linspace if binning=='lin' else np.geomspace)
+    with fits.open(*args, **kwargs) as f:
+        hd = f[0].header
+        dt = f[1].data
+        data = DataProfile(
+            R = binspace(hd['RIN'],hd['ROUT'],hd['N']),
+            redshift = hd['Z_MEAN'],
+            Njk = hd['NK'],
+            Sigma = dt['Sigma'],
+            DSigma_t = dt['DSigma_t'],
+            DSigma_x = dt['DSigma_x'],
+            covS = f[2].data,
+            covDSt = f[3].data,
+            covDSx = f[4].data,
+        )
+    return data
+
+def save_chains_h5():
+    pass
