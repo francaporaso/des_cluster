57c5fe2ba82e2472670d0665e6cdad45cbea6364
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-04-17 16:35:05 -0300

    update

--- cluster/inertia.py
+++ cluster/inertia.py
@@ -12,8 +12,8 @@ MEMBERS = Table.read('../cats/DESY3/desy3_redmapper_cluster-members.fits', forma
 
 def gnomonic_projection(ra, dec, ra0, dec0):
     '''
-    gnomonic projection of the shpere on a tangent point at some position.
-    for transforming spherical coordinates to the plane
+    gnomonic projection of the shpere on a tangent point at (ra0,dec0).
+    Maps spherical coordinates to a tangent plane.
     '''
     ra = np.deg2rad(ra-ra0)
     dec = np.deg2rad(dec-dec0)
@@ -26,7 +26,7 @@ def gnomonic_projection(ra, dec, ra0, dec0):
 def inertia(dx,dy,w):
     '''
     "momentos" func from github.com/elizabethjg/multipole_density_profile/blob/master/member_distribution.py
-    equations 2, 3 & 4 of Gonzalez et al 2020 (arxiv.org/pdf/2006.08651)
+    Equations 2, 3 & 4 of Gonzalez et al 2020 (arxiv.org/pdf/2006.08651)
     '''
 
     Q11  = np.sum((dx**2)*w)/np.sum(w)
@@ -38,30 +38,31 @@ def inertia(dx,dy,w):
     theta = np.arctan2(E2,E1)/2.0
     return e,theta
 
-def cluster_orientation(ra_cl, dec_cl, ra_mem, dec_mem):
-    
+def cluster_orientation(ra_cl, dec_cl, ra_mem, dec_mem, weight=None):
+    if not weight:
+        weight = np.ones_like(ra_mem)
     x, y = gnomonic_projection(ra_mem, dec_mem, ra_cl, dec_cl)
-    e, theta = inertia(x, y, np.ones_like(x))
+    e, theta = inertia(x, y, weight)
     return x, y, e, theta
 
 def main():
 
     l = CLUSTERS[ (CLUSTERS['lambda']>LMIN) & (CLUSTERS['lambda']<=LMAX) & (CLUSTERS['redshift']>ZMIN) & (CLUSTERS['redshift']<=ZMAX) ]
-    id_cl = l['mem_match_id'][12]
-    
-    m = MEMBERS[MEMBERS['mem_match_id']==id_cl]
+    id_cl = l['mem_match_id']
 
-    x, y, e, theta = cluster_orientation(l[l['mem_match_id']==id_cl]['ra_cl'], l[l['mem_match_id']==id_cl]['dec_cl'], m['ra_mem'], m['dec_mem'])
+    fig, ax = plt.subplots(1,1)    
+    for idx in id_cl:
+        m = MEMBERS[MEMBERS['mem_match_id']==idx]
+        x, y, e, theta = cluster_orientation(l[l['mem_match_id']==idx]['ra_cl'], l[l['mem_match_id']==idx]['dec_cl'], m['ra_mem'], m['dec_mem'])
 
-    print(f'{e=}')
-    print(f'{theta=}')
+        print(f'{e=}')
+        print(f'{theta=}')
 
-    plt.figure()
-    plt.scatter(x, y, s=10, edgecolor='b', facecolor='none')
-    plt.axline((0.0,0.0), slope=np.tan(theta), c='k')
-    plt.axline((0.0,0.0), slope=np.tan(theta+np.pi), c='k')
-    plt.show()
-
+        ax.figure()
+        ax.scatter(x, y, s=10, edgecolor='b', facecolor='none')
+        ax.axline((0.0,0.0), slope=np.tan(theta), c='k')
+        ax.axline((0.0,0.0), slope=np.tan(theta+np.pi), c='k')
+    fig.show()
 
 if __name__=='__main__':
     main()
