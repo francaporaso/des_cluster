4c2fd06dfce58dec37bff7d348408bb1f69fa7a5
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-04-23 12:56:44 -0300

    update: calc of the orientation and elongation with weights defined by Gonzalez+2020 and Gonzalez+2022

--- cluster/inertia.py
+++ cluster/inertia.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from astropy.cosmology import FlatLambdaCDM
 from astropy.table import Table
+from astropy.wcs import WCS
 import h5py
+
+COSMO = FlatLambdaCDM(H0=100.0, Om0=0.3)
 
 ZMIN, ZMAX = 0.19, 0.27
 LMIN, LMAX = 38.0, 55.0
 
-CLUSTERS = Table.read('../cats/DESY3/desy3_redmapper_cluster-ws.fits', format='fits', memmap=True)
-MEMBERS = Table.read('../cats/DESY3/desy3_redmapper_cluster-members.fits', format='fits', memmap=True)
+CLUSTERS = Table.read('../../cats/DESY3/desy3_redmapper_cluster-ws.fits', format='fits', memmap=True)
+MEMBERS = Table.read('../../cats/DESY3/desy3_redmapper_cluster-members.fits', format='fits', memmap=True)
 
 def gnomonic_projection(ra, dec, ra0:float, dec0:float):
     '''
@@ -23,15 +26,15 @@ def gnomonic_projection(ra, dec, ra0:float, dec0:float):
 
     return x, y
 
-def inertia(dx,dy,w):
+def inertia(dx,dy,weight):
     '''
     "momentos" func from github.com/elizabethjg/multipole_density_profile/blob/master/member_distribution.py
     Equations 2, 3 & 4 of Gonzalez et al 2020 (arxiv.org/pdf/2006.08651)
     '''
 
-    Q11  = np.sum((dx**2)*w)/np.sum(w)
-    Q22  = np.sum((dy**2)*w)/np.sum(w)
-    Q12  = np.sum((dx*dy)*w)/np.sum(w)
+    Q11  = np.sum((dx**2)*weight)/np.sum(weight)
+    Q22  = np.sum((dy**2)*weight)/np.sum(weight)
+    Q12  = np.sum((dx*dy)*weight)/np.sum(weight)
     E1 = (Q11-Q22)/(Q11+Q22)
     E2 = (2.*Q12)/(Q11+Q22)
     e = np.sqrt(E1**2 + E2**2)
@@ -47,18 +50,62 @@ def cluster_orientation(ra_cl, dec_cl, ra_mem, dec_mem, weight=None):
 
 def main():
 
+    wcs = WCS(naxis=2)
+    wcs.wcs.crpix=[0.0, 0.0]
+    wcs.wcs.cdelt=[1.0/3600.0, 1.0/3600.0]
+    wcs.wcs.cunit=['deg', 'deg']
+    wcs.wcs.ctype=['RA---TAN', 'DEC--TAN']
+
     #l = CLUSTERS[ (CLUSTERS['lambda']>LMIN) & (CLUSTERS['lambda']<=LMAX) & (CLUSTERS['redshift']>ZMIN) & (CLUSTERS['redshift']<=ZMAX) ]
     l = CLUSTERS
-    id_cl = l['mem_match_id']
+    nclusters = len(l)
+    id_cl = l['mem_match_id'].data
 
-    e = np.zeros(len(l))
-    theta = np.zeros(len(l))
+    samples_names = ['allmem', 'pmemcut']
+    weights_names = ['wo_weight', 'lum', 'dist']
+
+    e = {sam: {w: np.zeros(len(l)) for w in weights_names} for sam in samples_names}
+    theta = {sam: {w: np.zeros(len(l)) for w in weights_names} for sam in samples_names}
 
     for i, idx in enumerate(id_cl):
-        m = MEMBERS[MEMBERS['mem_match_id']==idx]
-        _, _, e[i], theta[i] = cluster_orientation(l[l['mem_match_id']==idx]['ra_cl'], l[l['mem_match_id']==idx]['dec_cl'], m['ra_mem'], m['dec_mem'])
+        l_idx = l[id_cl==idx]
 
-    np.savetxt('../cats/DESY3/redmapper_orientation.dat', np.vstack([id_cl, e, theta]), header='# id_cl | e | theta ')
+        mask_mem = MEMBERS['mem_match_id']==idx
+        mask_cen = MEMBERS['ra_mem']!=l_idx['ra_cl']
+        m = MEMBERS[mask_mem&mask_cen]
+
+        for sam in samples_names:
+
+            if sam=='pmemcut':
+                m = m[m['pmem']>0.5]
+
+            wcs.wcs.crval = list(l_idx['ra_cl', 'dec_cl'].as_array()[0])
+            x, y = wcs.wcs_world2pix(m['ra_mem'], m['dec_mem'], 0)
+
+            #plt.scatter(x,y,s=10,edgecolor=f'C{i}',facecolor=f'C{i}' if sam=='pmemcut' else 'none',label=i)
+
+            mag_abs_r = m['model_mag_r'].data+5.0-5.0*np.log10(COSMO.luminosity_distance(l_idx['redshift']).to('pc').value)
+            lum_r = 10.0**(-0.4*mag_abs_r)
+
+            weights = {
+                w: v for w,v in zip(weights_names, [np.ones(len(m)), lum_r, 1.0/np.hypot(x,y)])
+            }
+
+            for w in weights_names:
+                e[sam][w][i], theta[sam][w][i] = inertia(x, y, weight=weights[w])
+                #plt.axline([0.,0.],slope=np.tan(theta[sam][w][i]),ls='-' if sam=='pmemcut' else '--', c=f'C{i}')
+    #plt.legend(bbox_to_anchor=(1.5,1.5), ncol=5)
+
+
+    f = h5py.File('test-theta.hdf5', 'w')
+    for grp_name in theta:
+        grp = f.create_group(grp_name)
+        for dset_name in theta[grp_name]:
+            dset = grp.create_dataset(dset_name, data = theta[grp_name][dset_name])
+            print(grp_name, dset_name, theta[grp_name][dset_name])
+    f.close()
+
+    # np.savetxt('../cats/DESY3/redmapper_orientation.dat', np.vstack([id_cl, e, theta]), header='# id_cl | e | theta ')
 
 if __name__=='__main__':
     print('Start...')
