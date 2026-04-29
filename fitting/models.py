409384bb0b5c9958f9dfc5455ea1d1818b3d4e3b
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-04-29 14:26:18 -0300

    debug: NFWMiss now using self.sigma from its parent class

--- fitting/models.py
+++ fitting/models.py
@@ -211,14 +211,14 @@ class NFWMiss(NFW):
 
         def SNFW(r):
             # return Sigma_NFW_2h(r,z,M200,c200,cosmo_params=params,terms='1h')
-            return super().sigma(r, M200, c200)/1.e12
+            return /1.e12
 
         def S_RRs(Rs,R):
             # F_Eq13
             #argumento = lambda x: monopole(np.sqrt(R**2+Rs**2-2.*Rs*R*np.cos(x)))
             #integral  = integrate.quad(argumento, 0, 2.*np.pi, epsabs=1.e-01, epsrel=1.e-01)[0]
             x = np.linspace(0.,2.*np.pi,500)
-            integral = simpson(SNFW(np.sqrt(R**2 + Rs**2 -2.0 * Rs * R * np.cos(x))), x, even='first')
+            integral = simpson(self.sigma(np.sqrt(R**2 + Rs**2 -2.0 * Rs * R * np.cos(x)), M200, c200), x, even='first')
             return integral/(2.0 * np.pi)
         
         integral = np.zeros_like(R)
