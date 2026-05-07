54e511ab31ffaa902a207be426d6c8b58261593d
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2026-05-06 23:03:22 -0300

    update: nan of n_eff raises from jackknife. debug: checking for jackknife masks not empty

--- lensing/dsigma.py
+++ lensing/dsigma.py
@@ -235,7 +235,7 @@ def stacking(zmin, zmax, lmin, lmax, pcen=0.5):
 
     for j, k in enumerate(kunq):
         mask = (kidx!=k)
-
+        assert mask.sum()!=0
         dsigma_t_num[j+1,:] = gt[mask].sum(axis=0)
         dsigma_x_num[j+1,:] = gx[mask].sum(axis=0)
         response_sum[j+1,:] = res[mask].sum(axis=0)
@@ -246,7 +246,7 @@ def stacking(zmin, zmax, lmin, lmax, pcen=0.5):
     n_eff = weight_sl/sq_weight_sl
     print(np.sum(np.isnan(n_eff)))
     for i in range(1,localNJK+1):
-        print(np.sum(sq_weight_sl[j,:]))
+        print(np.sum(sq_weight_sl[i,:]))
     return 1
     #response = np.sum(response_sum, axis=0)
 
