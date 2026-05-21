29e78da798648561bfb79da9441857d0310c94c5
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-05-21 16:58:19 -0300

    update: reconfigured the Config class. update: added the option of secondhalo to fit. update: preparing for fitting quadrupole

--- fitting/main.py
+++ fitting/main.py
@@ -14,7 +14,29 @@ from fitting.utilfuncs import *
 from fitting.plotting import *
 
 # ==== Fix globals
-cfg = None
+
+models_dict = {
+    'NFW':NFW,
+}
+
+default_limits = {
+    'NFW':{
+        'M200':(1e10, 1e16),
+        # 'c200':(1.0, 10.0),
+        'pcc':(0.01,1.0),
+        # 's_off':(0.01,1.0)
+    },
+}
+
+default_guess = {
+    'NFW':{
+        'M200':1e14,
+        # 'c200':4.0,
+        'pcc':0.8,
+        # 's_off':0.4
+    },
+}
+
 
 # ====
 class Config:
@@ -23,23 +45,29 @@ class Config:
 
         config = toml.load(configfile)
 
-        self.DATANAME = config['NAMES']['DATANAME']
-        self.FOLDER = config['NAMES']['FOLDER']
-        self.SAMPLE = config['NAMES']['SAMPLE']
-        self.CHAINNAME = config['NAMES']['CHAINNAME']
-        self.ZBINS = self._edges_to_bins(config['LENSES']['ZEDGES'], 'ZEDGES')
-        self.LBINS = self._edges_to_bins(config['LENSES']['LEDGES'], 'LEDGES')
-        self.NBINS = config['LENSES']['NBINS']
-        self.BINNING = config['LENSES']['BINNING']
+        self.NCORES = config['run']['ncores']
+        self.OVERWRITE = config['run']['overwriteh5']
+        self.PLOT = config['run']['plot']
 
-        self.NCORES = config['RUN']['NCORES']
-        self.NIT = config['RUN']['NIT']
-        self.NWALKERS = config['RUN']['NWALKERS']
-        self.COVMODE = config['RUN']['COVMODE']
-        self.MODEL = config['RUN']['MODEL']
-        self.FIXPARAM = config['RUN']['FIXPARAM']
-        self.OBSERVABLE = config['RUN']['OBSERVABLE']
-        self.PLOT = config['RUN']['PLOT']
+        self.NIT = config['mcmc']['nit']
+        self.NWALKERS = config['mcmc']['nwalkers']
+        self.covmode = config['mcmc']['covmode']
+        self.model = config['mcmc']['model']
+        self.secondhalo = config['mcmc']['secondhalo']
+        self.observable = config['mcmc']['observable']
+        self.FIXPARAM = config['mcmc']['fixparam']
+        self.CHAINSAMPLE = config['mcmc']['chainsample']
+
+        self.FOLDER = config['profiles']['folder']
+        self.DATANAME = config['profiles']['dataname']
+        self.SAMPLE = config['profiles']['sample']
+        self.ZEDGES = self._edges_to_bins(config['profiles']['zedges'], 'ZEDGES')
+        self.LBINS = self._edges_to_bins(config['profiles']['ledges'], 'LEDGES')
+        self.PCEN = config['profiles']['pcen']
+        self.MEMCUT = config['profiles']['memcut']
+        self.WANG = config['profiles']['angweight']
+        self.NBINS = config['profiles']['nbins']
+        self.BINNING = config['profiles']['binning']
 
     def _edges_to_bins(self, edges, name):
         if not isinstance(edges, list) or len(edges) < 2:
@@ -55,11 +83,15 @@ def run_emcee(
         data_filename, save_filename,
         model_name='NFW',
         observable='delta_sigma',
+        secondhalo=False,
         fix_params = ['c200'],
         cov_mode='diag',
         ):
 
-    data = read_dataprofile_fits(name=data_filename)
+    if observable == 'delta_sigma':
+        data = vread_dataprofile_fits(name=data_filename, profile='monopole')
+    elif observable == 'quad_gamma_t':
+        data = vread_dataprofile_fits(name=data_filename, profile='quadrupole')
 
     param_limits = default_limits.get(model_name)
     init_guess = default_guess.get(model_name)
@@ -68,7 +100,7 @@ def run_emcee(
 
     L = Likelihood(
         data=data,
-        model=models_dict.get(model_name)(data.redshift),
+        model=models_dict.get(model_name)(redshift=data.redshift, secondhalo=secondhalo),
         param_limits=param_limits,
         observable=observable,
         cov_mode=cov_mode
@@ -80,7 +112,7 @@ def run_emcee(
        rng.uniform(ig*(1-0.2), ig*(1+0.2), nwalkers) for ig in init_guess.values()
     ]).T #ordering of dict is asserted in python >3.7
 
-    group_name = f'emcee/{model_name}/{cov_mode}'
+    group_name = f'emcee/{model_name}/{'1h+2h' if secondhalo else '1h'}/{cov_mode}'
     backend = emcee.backends.HDFBackend(save_filename, name=group_name)
     with Pool(processes=ncores) as pool:
         sampler = emcee.EnsembleSampler(
@@ -94,7 +126,6 @@ def main():
     global cfg
 
     parser = ArgumentParser()
-    parser.add_argument('--overwrite', action='store_true')
     parser.add_argument('--config', type=str, default='fitting/config.toml', action='store')
     args = parser.parse_args()
 
@@ -108,8 +139,8 @@ def main():
         zstr = f'z{100*zmin:03.0f}-{100*zmax:03.0f}'
         lstr = f'lambda{lmin:02.0f}-{lmax:02.0f}'
 
-        data_filename = self.FOLDER + f'{cfg.DATANAME}_{zstr}_{lstr}_bin{cfg.NBINS}{cfg.BINNING}.fits'
-        chain_filename = self.FOLDER + f'{cfg.CHAINNAME}_{cfg.SAMPLE}_{zstr}_{lstr}.hdf5'
+        data_filename = self.FOLDER + f'{cfg.DATANAME}_{cfg.SAMPLE}_{zstr}_{lstr}_bin{cfg.NBINS}{cfg.BINNING}.fits'
+        chain_filename = self.FOLDER + f'fitting_{cfg.CHAINSAMPLE}_{zstr}_{lstr}.hdf5'
 
         sampler = run_emcee(
             ncores=cfg.NCORES,
@@ -117,18 +148,21 @@ def main():
             nwalkers=cfg.NWALKERS,
             data_filename=data_filename,
             save_filename=chain_filename,
-            model_name=cfg.MODEL,
-            observable=cfg.OBSERVABLE,
+            model_name=cfg.model,
+            observable=cfg.observable,
             fix_params=cfg.FIXPARAM,
-            cov_mode=cfg.COVMODE,
+            cov_mode=cfg.covmode,
         )
 
-        param_names = list(default_limits.get(cfg.MODEL).keys())
+        param_names = list(default_limits.get(cfg.model).keys())
         # not possible to fix params for now
-        fitpar, errpar = get_fitted_params(sampler.get_chain(discard=int(cfg.NIT*0.15)), param_names)
+        fitpar, errpar = get_fitted_params(
+            sampler.get_chain(discard=int(cfg.NIT*0.15)), 
+            param_names
+        )
 
         with h5py.File(chain_filename, 'a') as f:
-            group_path = f'fitedparams/{cfg.MODEL}/{cfg.COVMODE}'
+            group_path = f'fitedparams/{cfg.model}/{cfg.covmode}'
 
             # Overwrite if exists
             if group_path in f:
